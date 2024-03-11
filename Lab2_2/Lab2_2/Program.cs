1a252681699ab953ced4dedaea76f91a3ec0d894
Nuss Eduard Andreevich <nuss.ea@arkan.ru>
2024-03-11 20:06:29 +0300

    add Lab2

---
+++ Lab2_2/Lab2_2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2_2
+{
+   internal class Program
+   {
+      static void Main(string[] args)
+      {
+         //1 Zadacha - Дана длина L окружности. Найти ее радиус R и площадь S круга, ограниченного этой окружностью.
+         Console.WriteLine();
+         double l = Convert.ToDouble(Console.ReadLine());
+         double r = (l / Math.PI) / 2;
+         double s = Math.Pow(l, 2) / (4 * Math.PI);
+         Console.WriteLine("Радиус круга: {0}", r);
+         Console.WriteLine("Площадь круга: {0}", s);
+         Console.ReadLine();
+      }
+   }
+}
