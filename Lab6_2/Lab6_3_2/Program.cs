adef15fcf2f9ff52b74a92e197a4dc47bb7770cb
Nuss Eduard Andreevich <nuss.ea@arkan.ru>
2024-03-11 19:44:08 +0300

    ref all

---
+++ Lab6_2/Lab6_3_2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6_3_2
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         // 2 zadacha - Ввести с клавиатуры предложение.
+         // Предложение представляет собой слова, разделенные пробелом.
+         // Знаки препинания не используются.
+         // Составить программу, определяющую является ли строка палиндромом без учёта пробелов и регистра (пример палиндрома – «А роза упала на лапу Азора»).
+         Console.Write("Введите предложение:");
+         string start = Console.ReadLine();
+         string[] x1 = start.Split();
+         string x2 = "";
+         string end = "";
+         foreach (string i in x1) x2 += i;
+         for (int i = 0; i < x2.Length; i++)
+         {
+            end = x2[i] + end;
+         }
+         if (end.ToLower() == x2.ToLower()) Console.WriteLine("Является палиндром");
+         else Console.WriteLine("Не является палиндром");
+         Console.ReadKey();
+      }
+   }
+}
--- Lab6_2/Lab6_3_dz_1_2/Program.cs
+++ Lab6_2/Lab6_3_dz_1_2/Program.cs
@@ -10,8 +10,12 @@ namespace Lab6_3_dz_1_2
    {
       static void Main(string[] args)
       {
-         // 1 zadacha - максимальное слово в предложении
-         /*string start = Console.ReadLine();
+         // 1 zadacha - Ввести с клавиатуры предложение.
+         // Предложение представляет собой слова, разделенные пробелом.
+         // Знаки препинания не используются.
+         // Найти самое длинное слово в строке.
+         Console.Write("Введите предложение:");
+         string start = Console.ReadLine();
          string max = "";
          string[] mass;
          mass = start.Split();
@@ -24,27 +28,7 @@ namespace Lab6_3_dz_1_2
                max = i;
             }
          }
-         Console.WriteLine(max);
-         Console.ReadKey();*/
-
-         // 2 zadacha - перевернутое сообщение
-         /*string start = Console.ReadLine();
-         string[] x1 = start.Split();
-         string x2 = "";
-         string end = "";
-         foreach (string i in x1) x2 += i;
-         for (int i = 0; i < x2.Length; i++)
-         {
-            end = x2[i] + end;
-         }
-         if (end.ToLower() == x2.ToLower()) Console.WriteLine("Является палиндром");
-         else Console.WriteLine("Не является палиндром");
-         Console.ReadKey();*/
-         // 3 zadacha
-         string s = Console.ReadLine();
-         string[] s1 = s.Split();
-         
-         Console.WriteLine(s1);
+         Console.Write("Самое длинное слово в предложении: {0}", max);
          Console.ReadKey();
       }
    }
