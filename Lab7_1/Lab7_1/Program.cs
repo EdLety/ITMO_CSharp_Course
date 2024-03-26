eb407df19276f6526f5b7e2726e14ee26d561b46
Nuss Eduard Andreevich <nuss.ea@arkan.ru>
2024-03-26 10:24:53 +0300

    add lab 7 HW and fix 11 HW

---
+++ Lab7_1/Lab7_1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7_1
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         int a1 = Convert.ToInt32(Console.ReadLine());
+         int b1 = Convert.ToInt32(Console.ReadLine());
+
+         int a2 = Convert.ToInt32(Console.ReadLine());
+         int b2 = Convert.ToInt32(Console.ReadLine());
+         if (FindGipoten(a1, b1) == FindGipoten(a2, b2)) Console.WriteLine("Гипотенузы равны");
+         else Console.WriteLine("Гипотенузы не равны");
+         double r = Convert.ToDouble(Console.ReadLine());
+         double v, s;
+         FindVandS(r, out v, out s);
+         Console.WriteLine("{0} {1}", s, v);
+         Console.ReadKey();
+      }
+      static double FindGipoten(double b, double a) 
+      {
+         double c = Math.Sqrt(Math.Pow(b, 2) + Math.Pow(a, 2));
+         return c;
+      }
+      static void FindVandS(double r, out double v, out double s)
+      {
+         s = Math.PI * 4 * Math.Pow(r, 2); 
+         v = (double)4 / 3 * Math.PI * Math.Pow(r, 3);
+      }
+   }
+}
---
+++ Lab7_1/Lab7_2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7_2
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         Console.WriteLine("Введите 3 длины сторон 1 треугольника: ");
+         double x1 = Convert.ToDouble(Console.ReadLine());
+         double y1 = Convert.ToDouble(Console.ReadLine());
+         double z1 = Convert.ToDouble(Console.ReadLine());
+         Console.WriteLine("Введите 3 длины сторон 2 треугольника:");
+         double x2 = Convert.ToDouble(Console.ReadLine()); 
+         double y2 = Convert.ToDouble(Console.ReadLine()); 
+         double z2 = Convert.ToDouble(Console.ReadLine());
+         FindSMax(x1, y1, z1, out double s1);
+         FindSMax(x2, y2, z2, out double s2);
+
+         if (s1 > s2) Console.WriteLine("1 треугольник больше 2 треугольника");
+         else if (s2 > s1) Console.WriteLine("2 треугольник больше 1 треугольника");
+         else Console.WriteLine("Треугольники равны");
+         Console.ReadKey();
+      }
+      // 1 zadacha
+      static void FindSMax(double x1, double y1, double z1,out double s1)
+      {
+         double p1 = (x1 + y1 + z1) / 2.0;
+         s1 = Math.Sqrt(p1 * (p1 - x1) * (p1 - y1) * (p1 - z1));
+      }
+   }
+}
