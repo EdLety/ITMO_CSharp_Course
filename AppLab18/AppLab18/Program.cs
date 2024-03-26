fb0ca1b0e1e30f415186aef056c5951c8f405405
Nuss Eduard Andreevich <nuss.ea@arkan.ru>
2024-03-26 09:36:13 +0300

    Add 18 HW

---
+++ AppLab18/AppLab18/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace AppLab18
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         
+         /*List<int> list = new List<int>();
+         list.Add(1);
+         list.Add(2);
+         list.Add(3);
+         int[] array = new int[3] { 4, 5, 6 };
+         list.AddRange(array);
+         list.RemoveAt(3);
+         list.Reverse();
+         for (int i = 0; i < list.Count; i++)
+         {
+            list[i]++;
+         }
+         foreach (int item in list)
+         {
+            Console.WriteLine(item);
+         }
+
+         LinkedList<int> lList = new LinkedList<int>();
+         lList.AddLast(1);
+         lList.AddLast(2);
+         lList.AddLast(3);
+         LinkedListNode<int> node = lList.First;
+         Console.WriteLine(node);
+
+         Dictionary<string, int> keys = new Dictionary<string, int>();
+         keys.Add("jan", 1);
+         keys.Add("feb", 2);
+         keys.Add("mar", 3);
+         keys.Add("apr", 4);
+         keys.Add("may", 5);
+         keys.Add("jun", 6);
+         keys.Add("jul", 7);
+         keys.Add("aug", 8);
+         keys.Add("sep", 9);
+         keys.Add("oct", 10);
+         keys.Add("nov", 11);
+         keys.Add("dec", 12);
+         Console.WriteLine(keys["may"]);*/
+
+
+         Console.ReadKey();
+      }
+   } 
+}
